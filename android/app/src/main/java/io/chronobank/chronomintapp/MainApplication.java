edbfbed3cfce3f2cc852e44d714525a36d45d7ce
Igor Pavlenko <ipavlenko@inbox.ru>
2017-11-22 23:09:00 +0500

    Android support

--- android/app/src/main/java/io/chronobank/chronomintapp/MainApplication.java
+++ android/app/src/main/java/io/chronobank/chronomintapp/MainApplication.java
@@ -1,46 +1,32 @@
 package io.chronobank.chronomintapp;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
+import com.reactnativenavigation.NavigationApplication;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NavigationReactPackage(),
-            new RNDeviceInfo()
-            new ReactNativeLocalizationPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+        new RNDeviceInfo(),
+        new ReactNativeLocalizationPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
   }
 
   @Override
